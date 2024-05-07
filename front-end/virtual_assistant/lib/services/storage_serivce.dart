74818bdb2404327614b74ce7a12ed65cd98fbd61
abdeslam moukhliss <abdomokh38@gmail.com>
2024-05-07 14:22:17 +0100

    add local storage

--- front-end/virtual_assistant/lib/services/storage_serivce.dart
+++ front-end/virtual_assistant/lib/services/storage_serivce.dart
@@ -1,79 +1,61 @@
-// import 'package:hive/hive.dart';
-// import 'package:hive_flutter/hive_flutter.dart';
-//
-// class StorageService {
-//   late Box _box;
-//
-//   Future<void> init() async {
-//     await Hive.initFlutter();
-//     Hive.registerAdapter(MessageAdapter());
-//     _box = await Hive.openBox('messages');
-//   }
-//
-//   Future<void> writeData(List<Message> messages) async {
-//     await _box.put('messages', messages.map((m) => m.toJson()).toList());
-//   }
-//
-//   Future<List<Message>> readData() async {
-//     var data = _box.get('messages', defaultValue: []) as List;
-//     return data.map((m) => Message.fromJson(m)).toList();
-//   }
-// }
-// part 'message.g.dart'; // Name of the generated file
-//
-// @HiveType(typeId: 0)
-// class Message {
-//   // ... existing code ...
-//
-//   @HiveField(0)
-//   String? id;
-//
-//   @HiveField(1)
-//   String? message;
-//
-//   @HiveField(2)
-//   bool isSender;
-//
-//   @HiveField(3)
-//   bool isSent;
-//
-//   @HiveField(4)
-//   DateTime? timestamp;
-//
-//   // ... existing code ...
-//
-//   Map<String, dynamic> toJson() {
-//     return {
-//       'id': id,
-//       'message': message,
-//       'isSender': isSender,
-//       'isSent': isSent,
-//       'timestamp': timestamp?.toIso8601String(),
-//     };
-//   }
-//
-//   factory Message.fromJson(Map<String, dynamic> json) {
-//     return Message(
-//       id: json['id'],
-//       message: json['message'],
-//       isSender: json['isSender'],
-//       isSent: json['isSent'],
-//       timestamp: DateTime.parse(json['timestamp']),
-//     );
-//   }
-// }
-//
-// class MessageAdapter extends TypeAdapter<Message> {
-//   @override
-//   final typeId = 0;
-//
-//   @override
-//   Message read(BinaryReader reader) {
-//     return Message.fromJson(reader.read());
-//   }
-//
-//   @override
-//   void write(BinaryWriter writer, Message obj) {
-//     writer.write(obj.toJson());
-//   }
-// }
+import 'dart:ffi';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:path_provider/path_provider.dart';
+
+import '../models/message.dart';
+import '../models/message_adapter.dart';
+
+class StorageService {
+  late Box<Map> _box;
+  static final StorageService _instance = StorageService._();
+
+  // Private constructor
+  StorageService._();
+
+  // Singleton instance
+  static StorageService get instance {
+    return _instance;
+  }
+  Future<void> init() async {
+    final appDocumentDir = await getApplicationDocumentsDirectory();
+    await Hive.initFlutter();
+      await Hive
+      ..init(appDocumentDir.path)
+      ..registerAdapter(MessageAdapter());
+    _box = await Hive.openBox<Map>('messages');
+    int a=0;
+  }
+
+  Future<void> writeData(List<Message> messages) async {
+    await _box.putAll(messages.map((e) => e.toJson()).toList().asMap());
+    return;
+  }
+  Future<List<Message>> readData() async {
+    var data = _box.values;
+    return data.map((e) => Message.fromJson(e.cast<String, dynamic>()) ).toList();
+
+  }
+  Future<void> saveMessage (Message message) {
+    return _box.put(_box.keys.length, message.toJson()) ;
+  }
+  ValueListenable<Box<dynamic>?>   getListener() {
+    return _box.listenable();
+  }
+   Future deleteLastMessage() {
+   return  _box.delete( _box.keys.last);
+  }
+  updateLastMessage(Message message) async{
+    await deleteLastMessage();
+   await  saveMessage(message);
+  }
+  Future clear() async {
+    await _box.clear();
+  }
+
+
+
+}
