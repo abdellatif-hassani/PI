3aebe112730b0d4629149ad17203b7e7f54a3555
Abderrazzak NOUARI <abderrazzak.nouari@gmail.com>
2024-05-11 16:05:55 +0100

    disabling the break point /exucute

--- back-end/Promp_GPT/src/main/java/com/example/promp_gpt/service/OpenAiService.java
+++ back-end/Promp_GPT/src/main/java/com/example/promp_gpt/service/OpenAiService.java
@@ -5,15 +5,12 @@ import com.example.promp_gpt.entities.GmailApiDto;
 import com.example.promp_gpt.entities.PromptResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
-
-
 public interface OpenAiService {
-   PromptResponse getPrompt(String userText,String systemText) throws JsonProcessingException;
-   GmailApiDto sendToTheGemailService(GmailApiDto gmailApiDto, String methodeToUse, String token);
+
+    PromptResponse getPrompt(String userText, String systemText) throws JsonProcessingException;
+    GmailApiDto sendToTheGemailService(GmailApiDto gmailApiDto, String methodeToUse, String token);
     Object sendToTheCalenderService(EventEntity eventEntity, String methodeToUse, String token);
-
-
-
+    
     Object sendToTheCorrectService(PromptResponse promptResponse, String token);
 
     PromptResponse getRePrompt(PromptResponse promptResponse,String userText,String systemText) throws JsonProcessingException;
