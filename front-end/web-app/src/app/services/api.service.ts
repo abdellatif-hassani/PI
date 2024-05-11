bbac58339118b2f1e29b36db7b326ecc93dd6eb2
SasboHassani <abdellatifhassani1205@gmail.com>
2024-05-11 16:13:34 +0100

    front-end: testing types of responses

--- front-end/web-app/src/app/services/api.service.ts
+++ front-end/web-app/src/app/services/api.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { AnyResponse } from '../models/response-types';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  
   private apiUrl = 'http://localhost:9090/prompt_service'; 
 
   constructor(private http: HttpClient) {}
@@ -23,7 +25,22 @@ export class ApiService {
         throw error;
       })
     );
+  }
 
+  sendEmail(arg0: AnyResponse | undefined, token: any): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`
+    });
+    console.log('authorization:', headers.get('Authorization'));
+    return this.http.post<any>(`${this.apiUrl}/email`, arg0, { headers }).pipe(
+      catchError(error => {
+        console.error('Error sending email:', error);
+        throw error;
+      })
+    );
     
   }
+
+
 }
